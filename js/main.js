82bd4b260846cf0c00b16ce5abf7df73f6d7d578
okorojames <okorojameschizaram@gmail.com>
2023-01-20 04:51:15 +0100

    ci/cd

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ function onSubmitLink(e) {
     alert('not a valid link')
     document.querySelector('.input-link-input').value = ""
   } else {
-    console.log(linkInputBox)
+    getShortLink(linkInputBox)
     document.querySelector('.input-link-input').value = ""
   }
 }
@@ -27,3 +27,20 @@ function onSubmitLink(e) {
 //
 // FETCH API FUNCTION CODE
 // 
+async function getShortLink(shortedLink) {
+  const response = await fetch(`https://api.shrtco.de/v2/shorten?url=${shortedLink}`);
+  const data = await response.json();
+  if (response.ok) {
+    linksGenerated += `
+    
+      <div class="linkGenerated">
+        <div class="longLink"><a ${linkInputBox}" class="long-link">${linkInputBox}</a></div>
+        <div class="shortLink">
+          <div class="short-link"><a ${data.original_link}" class="short--link">${data.full_short_link}</a></div>
+          <button class="copy-btn">Copy</button>
+        </div>
+      </div>
+    
+    `
+  }
+}
